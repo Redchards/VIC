2f809574a8269f2b906b9a07b91124e0cce7ea69
Rechards <urien.loic.cours@gmail.com>
2018-04-18 21:57:44 +0200

    implements the first agent strategy

--- ElectiSim/src/main/java/org/upmc/electisim/OmniscientBestResponseStrategy.java
+++ ElectiSim/src/main/java/org/upmc/electisim/OmniscientBestResponseStrategy.java
@@ -1,7 +1,10 @@
 package org.upmc.electisim;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class OmniscientBestResponseStrategy extends ABestReponseAgentStrategy {
 
@@ -12,8 +15,29 @@ public class OmniscientBestResponseStrategy extends ABestReponseAgentStrategy {
 	@Override
 	public VoteResult executeVote(Agent agent, List<Candidate> candidateList, int committeeSize) {
 		List<List<Candidate>> possibleCommittees = generateCombinations(candidateList, committeeSize);	
-		return null;
-		//List<Candidate> currentBestCommittee
+
+		List<Candidate> currentBestCommittee = new ArrayList<>();
+		int bestDist = -1;
+		
+		for(List<Candidate> committee : possibleCommittees) {
+			Optional<Integer> dist = agent.getPreferences().getCommitteeDistance(committee);
+			if(dist.get() < bestDist) {
+				currentBestCommittee = committee;
+				bestDist = dist.get();
+			}
+		}
+		
+		Map<Candidate, Integer> scoreMap = new HashMap<>();
+		
+		for(Candidate c : candidateList) {
+			scoreMap.put(c, 0);
+		}
+		
+		for(Candidate c : currentBestCommittee) {
+			scoreMap.put(c, 1);
+		}
+		
+		return new VoteResult(agent, scoreMap);
 	}
 	
 	private List<List<Candidate>> generateCombinations(List<Candidate> candidateList, int committeeSize) {
