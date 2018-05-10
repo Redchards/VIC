6e190448d14fba9ef7b20996a45525544bbcddc3
Yasmine <hamdaniyasmine15@gmail.com>
2018-05-10 22:46:21 +0200

    lexicographic tie-breaking for BlocVotingRule added

--- ElectiSim/src/main/java/org/upmc/electisim/BlocVotingRule.java
+++ ElectiSim/src/main/java/org/upmc/electisim/BlocVotingRule.java
@@ -15,26 +15,38 @@ public class BlocVotingRule implements IVotingRule {
 	public ElectionResult getElectionResult(List<AgentVote> results, int committeeSize) {
 		Map<IElectable, Integer> scores = new HashMap<>();
 		List<IElectable> electedCommittee = new ArrayList<>();
-		
+
 		for(IElectable c : results.get(0).getScoreMap().keySet()) {
 			scores.put(c,  0);
 		}
-		
+
 		for(AgentVote res : results) {
 			for(IElectable c : res.getKBests(committeeSize)) {
 				scores.put(c, scores.get(c).intValue() + 1);
 			}
 		}
-		
+
 		List<Map.Entry<IElectable, Integer>> set = MapUtils.sortByValue(scores);
-		Collections.reverse(set);
-		
-		
-        for(Map.Entry<IElectable, Integer> c : set.subList(0, committeeSize)) {
-        	electedCommittee.add(c.getKey());
-        }
-        
-        return new ElectionResult(scores, electedCommittee);
+		//Collections.reverse(set);
+
+		set.sort(new Comparator<Map.Entry<IElectable,Integer>>() {
+
+			@Override
+			public int compare(Map.Entry<IElectable, Integer> c_score1, Map.Entry<IElectable, Integer> c_score2) {
+				int comp_by_value = c_score2.getValue().compareTo(c_score1.getValue());
+				if (comp_by_value != 0) {
+					return comp_by_value;
+				} else {
+					return c_score1.getKey().getName().compareTo(c_score2.getKey().getName());
+				}
+			}
+		});
+
+		for(Map.Entry<IElectable, Integer> c : set.subList(0, committeeSize)) {
+			electedCommittee.add(c.getKey());
+		}
+
+		return new ElectionResult(scores, electedCommittee);
 	}
 
 }
